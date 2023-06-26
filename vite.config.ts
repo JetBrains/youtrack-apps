8a5cfb81ea688d61e7b4f1574431ef1144ca75dd
andrey.volfman <andrey.volfman@jetbrains.com>
2023-06-26 11:35:39 +0300

    add package.json

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    eslint({
+      failOnError: false,
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'manifest.json',
+          dest: '../'
+        },
+        {
+          src: 'public/*',
+          dest: '../'
+        },
+      ],
+    }),
+  ],
+  base: './',
+  publicDir: '',
+  build: {
+    outDir: 'dist/widgets',
+    target: ['es2022']
+  },
+})
