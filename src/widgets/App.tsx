ab2cd9d6cd731616c74092a02df9dbd1d504ff98
andrey.volfman <andrey.volfman@jetbrains.com>
2023-06-26 11:41:45 +0300

    push youtrack-votes-plugin

--- src/widgets/App.tsx
+++ src/widgets/App.tsx
@@ -4,33 +4,29 @@ import { ControlsHeightContext } from '@jetbrains/ring-ui/dist/global/controls-h
 import { Histogram } from './components/Histogram';
 import { ControlsHeight } from '@jetbrains/ring-ui/components/global/controls-height';
 
-// // @ts-expect-error
-// const host = await YTPlugin.register();
-//
-// // @ts-expect-error
-// const issueID: string = YTPlugin.entity.id;
-//
-// // @ts-expect-error
-// const created = new Date(YTPlugin.entity.created).getTime();
-const created = 1687766058;
+// @ts-expect-error
+const host = await YTPlugin.register();
 
-// const issue = await host.fetchYouTrack(`issues/${issueID}/activitiesPage`, {
-//     query: {
-//         categories: 'VotersCategory',
-//         fields: 'activities(timestamp)',
-//         top: 1000,
-//     },
-// });
-//
-// const data = issue.activities.map((activity: { timestamp: number }) => activity.timestamp);
-const data = Array<number>();
+// @ts-expect-error
+const issueID: string = YTPlugin.entity.id;
+
+// @ts-expect-error
+const created = new Date(YTPlugin.entity.created).getTime();
+
+const issue = await host.fetchYouTrack(`issues/${issueID}/activitiesPage`, {
+    query: {
+        categories: 'VotersCategory',
+        fields: 'activities(timestamp)',
+        top: 1000,
+    },
+});
+
+const data = issue.activities.map((activity: { timestamp: number }) => activity.timestamp);
 
 function App(): JSX.Element {
     return (
         <ControlsHeightContext.Provider value={ControlsHeight.S}>
-            <div style={{width: 400, height: 270, overflow: "hidden"}}>
-                <Histogram data={data} width={400} height={220} created={created} />
-            </div>
+            <Histogram data={data} width={400} height={220} created={created} />
         </ControlsHeightContext.Provider>
     );
 }
