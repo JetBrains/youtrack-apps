c093b4867c5a9b9a5daebc014f52a1d4789969f1
andrey.volfman <andrey.volfman@jetbrains.com>
2023-06-27 13:50:05 +0300

    declare global type for YTPlugin

--- src/widgets/App.tsx
+++ src/widgets/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import type { FC } from 'react';
 import '@jetbrains/ring-ui/dist/style.css';
 import { ControlsHeightContext } from '@jetbrains/ring-ui/dist/global/controls-height';
 import { Histogram } from './components/Histogram';
 import { ControlsHeight } from '@jetbrains/ring-ui/components/global/controls-height';
 
-// @ts-expect-error
+interface YTPluginHost {
+    fetchYouTrack: (path: string, options?: { query?: Record<string, string | number> }) => Promise<any>;
+}
+
+declare const YTPlugin: {
+    register: () => Promise<YTPluginHost>;
+    entity: {
+        id: string;
+        created: string;
+    };
+};
+
 const host = await YTPlugin.register();
 
-// @ts-expect-error
 const issueID: string = YTPlugin.entity.id;
 
-// @ts-expect-error
 const created = new Date(YTPlugin.entity.created).getTime();
 
 const issue = await host.fetchYouTrack(`issues/${issueID}/activitiesPage`, {
@@ -23,12 +33,12 @@ const issue = await host.fetchYouTrack(`issues/${issueID}/activitiesPage`, {
 
 const data = issue.activities.map((activity: { timestamp: number }) => activity.timestamp);
 
-function App(): JSX.Element {
+const App: FC = () => {
     return (
         <ControlsHeightContext.Provider value={ControlsHeight.S}>
             <Histogram data={data} width={636} height={360} created={created} />
         </ControlsHeightContext.Provider>
     );
-}
+};
 
 export default App;
