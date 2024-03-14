08c9985fa8b490fd551470ce2bd037b606ba28b3
andrey.volfman <andrey.volfman@jetbrains.com>
2024-03-14 16:20:49 +0200

    JT-79773 Fix data mismatch

--- src/widgets/api/fetch-votes.ts
+++ src/widgets/api/fetch-votes.ts
@@ -2,10 +2,12 @@ export async function fetchVotes(host: YTPluginHost, issueID: string): Promise<n
     const issue = await host.fetchYouTrack(`issues/${issueID}/activitiesPage`, {
         query: {
             categories: 'VotersCategory',
-            fields: 'activities(timestamp)',
+            fields: 'activities(timestamp,added)',
             $top: 1000,
         },
     });
 
-    return issue.activities.map((activity: { timestamp: number }) => activity.timestamp);
+    return issue.activities.flatMap((activity: {timestamp: number; added: unknown[]}) => {
+        return new Array(activity.added.length).fill(activity.timestamp);
+    });
 }
