13e307aebfc7961afa6fb64801cc9a7caf2f3c4a
Max Maximov <max.maximov@gmail.com>
2024-05-15 04:48:20 +0200

    Collect feedback from users

--- article-feedback/public/backend.js
+++ article-feedback/public/backend.js
@@ -1,19 +1,27 @@
-/* eslint-disable func-names */
-function response(ctx) {
-  return ctx.response.json({
-    likes: JSON.parse(ctx.article.extensionProperties.likes),
-    currentUser: ctx.currentUser.ringId
-  });
-}
-
 exports.httpHandler = {
   endpoints: [
     {
       scope: 'article',
       method: 'GET',
-      path: 'likes',
-      handle: function (ctx) {
-        response(ctx);
+      path: 'debug',
+      handle: function handleDebug(ctx) {
+        ctx.response.json(JSON.parse(ctx.article.extensionProperties.likes));
+      }
+    },
+
+    {
+      scope: 'article',
+      method: 'GET',
+      path: 'user',
+      handle: function handleUser(ctx) {
+        const likes = JSON.parse(ctx.article.extensionProperties.likes);
+        const userId = ctx.currentUser.ringId;
+        const isGuest = ctx.currentUser.login === 'guest';
+        ctx.response.json({
+          liked: likes[userId]?.liked ?? undefined,
+          leftMessage: Boolean(likes[userId]?.message),
+          isGuest
+        });
       }
     },
 
@@ -21,13 +29,17 @@ exports.httpHandler = {
       scope: 'article',
       method: 'POST',
       path: 'like',
-      handle: function (ctx) {
+      handle: function handleLike(ctx) {
+        if (ctx.currentUser.login === 'guest') {
+          return;
+        }
+
         const likes = JSON.parse(ctx.article.extensionProperties.likes);
+        const userId = ctx.currentUser.ringId;
         ctx.article.extensionProperties.likes = JSON.stringify({
           ...likes,
-          [ctx.currentUser.ringId]: true
+          [userId]: {liked: true, timestamp: Date.now()}
         });
-        response(ctx);
       }
     },
 
@@ -35,13 +47,40 @@ exports.httpHandler = {
       scope: 'article',
       method: 'POST',
       path: 'dislike',
-      handle: function (ctx) {
+      handle: function handleDislike(ctx) {
+        if (ctx.currentUser.login === 'guest') {
+          return;
+        }
+
         const likes = JSON.parse(ctx.article.extensionProperties.likes);
+        const userId = ctx.currentUser.ringId;
         ctx.article.extensionProperties.likes = JSON.stringify({
           ...likes,
-          [ctx.currentUser.ringId]: false
+          [userId]: {liked: false, timestamp: Date.now()}
         });
-        response(ctx);
+      }
+    },
+
+    {
+      scope: 'article',
+      method: 'POST',
+      path: 'feedback',
+      handle: function handleFeedback(ctx) {
+        if (ctx.currentUser.login === 'guest') {
+          return;
+        }
+
+        const likes = JSON.parse(ctx.article.extensionProperties.likes);
+        const userId = ctx.currentUser.ringId;
+        const message = ctx.request.getParameter('message');
+        ctx.article.extensionProperties.likes = JSON.stringify({
+          ...likes,
+          [userId]: {
+            liked: false,
+            message,
+            timestamp: Date.now()
+          }
+        });
       }
     }
   ]
--- article-feedback/widgets/feedback-form/app.tsx
+++ article-feedback/widgets/feedback-form/app.tsx
@@ -1,32 +1,82 @@
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 import type {FC} from 'react';
 import {ControlsHeightContext, ControlsHeight} from '@jetbrains/ring-ui-built/components/global/controls-height';
 import Button from '@jetbrains/ring-ui-built/components/button/button';
 import ButtonSet from '@jetbrains/ring-ui-built/components/button-set/button-set';
+import Input, {Size} from '@jetbrains/ring-ui-built/components/input/input';
 
 const host = await YTApp.register();
 
-const likes = await host.fetchApp('backend/likes', {scope: true});
-console.log('likes', likes);
+const debug = await host.fetchApp('backend/debug', {scope: true});
+console.log('debug', debug);
+
+const user = (await host.fetchApp('backend/user', {scope: true})) as {
+  isGuest: boolean;
+  liked?: boolean;
+  leftMessage?: boolean;
+};
 
 const AppComponent: FC = () => {
+  const [liked, setLiked] = useState(user.liked);
+  const [leftMessage, setLeftMessage] = useState(user.leftMessage);
+  const [message, setMessage] = useState('');
+
   const onLike = async () => {
-    const response = await host.fetchApp('backend/like', {scope: true, method: 'post'});
-    console.log('onLike', response);
+    await host.fetchApp('backend/like', {scope: true, method: 'post'});
+    setLiked(true);
   };
 
   const onDislike = async () => {
-    const response = await host.fetchApp('backend/dislike', {scope: true, method: 'post'});
-    console.log('onDislike', response);
+    await host.fetchApp('backend/dislike', {scope: true, method: 'post'});
+    setLiked(false);
+  };
+
+  const onSend = async () => {
+    await host.fetchApp('backend/feedback', {scope: true, method: 'post', query: {message}});
+    setLeftMessage(true);
+  };
+
+  const onMessageChange = async (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setMessage(event.target.value);
   };
 
   return (
     <ControlsHeightContext.Provider value={ControlsHeight.S}>
-      <p>{'Was this article helpful?'}</p>
-      <ButtonSet>
-        <Button onClick={onLike}>{'Yes'}</Button>
-        <Button onClick={onDislike}>{'No'}</Button>
-      </ButtonSet>
+      <div className="widget">
+        {liked === undefined && (
+          <>
+            <div>{'Was this article helpful?'}</div>
+            <ButtonSet>
+              <Button onClick={onLike}>{'Yes'}</Button>
+              <Button onClick={onDislike}>{'No'}</Button>
+            </ButtonSet>
+          </>
+        )}
+
+        {(liked === true || leftMessage) && (
+          <div>{'Thanks for your feedback!'}</div>
+        )}
+
+        {liked === false && !leftMessage && (
+          <>
+            <div>{'How can we improve?'}</div>
+            <Input
+              multiline
+              value={message}
+              onChange={onMessageChange}
+              size={Size.L}
+              className="message"
+            />
+            <ButtonSet>
+              <Button disabled={!message} onClick={onSend}>
+                {'Send'}
+              </Button>
+            </ButtonSet>
+          </>
+        )}
+      </div>
     </ControlsHeightContext.Provider>
   );
 };
