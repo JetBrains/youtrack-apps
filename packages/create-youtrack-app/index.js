390585388e0bf1fdd3baafceb48266a32aaf6a04
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-08-19 17:59:12 +0200

    Add simple index runner for hugen

---
+++ packages/create-youtrack-app/index.js
@@ -0,0 +1,16 @@
+const { runner } = require('hygen')
+const Logger = require('hygen/dist/logger')
+const path = require('path')
+const defaultTemplates = path.join(__dirname, 'templates')
+
+runner(process.argv.slice(2), {
+  templates: defaultTemplates,
+  cwd: process.cwd(),
+  logger: new Logger.default(console.log.bind(console)),
+  createPrompter: () => require('enquirer'),
+  exec: (action, body) => {
+    const opts = body && body.length > 0 ? { input: body } : {}
+    return require('execa').shell(action, opts)
+  },
+  debug: !!process.env.DEBUG
+})
