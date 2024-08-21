62ad043d48c364c13f87e4c708d8949958d56b21
Sergey Kochetkov <skoch13@pm.me>
2024-08-21 18:32:10 +0200

    chore(eslint): migrate to v9

---
+++ packages/apps-tools/eslint.config.mjs
@@ -0,0 +1,58 @@
+import globals from 'globals';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends('prettier'),
+  {
+    files: ['*.ts'],
+    plugins: {},
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+
+      ecmaVersion: 2023,
+      sourceType: 'commonjs',
+    },
+
+    rules: {
+      'one-var': [2, 'never'],
+      camelcase: 2,
+      'new-cap': 0,
+      'no-unreachable': 2,
+      'no-extra-boolean-cast': 2,
+      'valid-jsdoc': 0,
+      'no-lonely-if': 2,
+      'no-bitwise': 2,
+      'no-div-regex': 2,
+      curly: 0,
+      eqeqeq: 2,
+      yoda: 2,
+      'no-caller': 2,
+      'no-dupe-args': 2,
+      'no-debugger': 2,
+      'no-console': 0,
+      'no-shadow-restricted-names': 2,
+      'no-label-var': 2,
+      'no-delete-var': 2,
+      'no-use-before-define': [2, 'nofunc'],
+      'no-undef': 2,
+      'no-unused-vars': 2,
+      'no-undef-init': 2,
+      'handle-callback-err': 2,
+    },
+  },
+];
