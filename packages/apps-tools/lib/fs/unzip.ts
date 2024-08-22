adee5a70a5c402aabc6603839e6cb851e6a1b925
Sergey Kochetkov <skoch13@pm.me>
2024-08-22 19:30:25 +0200

    refactor(unzip): use non-legacy Open.file method

--- packages/apps-tools/lib/fs/unzip.ts
+++ packages/apps-tools/lib/fs/unzip.ts
@@ -2,22 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import unzipper from 'unzipper';
 
-export function unzip(zipFilePath: string, outputdir: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    if (!fs.existsSync(zipFilePath)) {
-      const error = new Error(`File "${zipFilePath}" not found`);
-      reject(error);
-    }
+export async function unzip(zipFilePath: string, outputdir: string): Promise<void> {
+  if (!fs.existsSync(zipFilePath)) {
+    const error = new Error(`File "${zipFilePath}" not found`);
+    throw error;
+  }
 
-    const outputPath = outputdir || path.dirname(zipFilePath);
-
-    fs.createReadStream(zipFilePath)
-      .pipe(
-        unzipper.Extract({
-          path: outputPath,
-        }),
-      )
-      .on('error', error => reject(error))
-      .on('close', () => resolve());
-  });
+  const outputPath = outputdir || path.dirname(zipFilePath);
+  const directory = await unzipper.Open.file(zipFilePath);
+  return await directory.extract({path: outputPath});
 }
