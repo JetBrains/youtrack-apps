c8b881cb4b9d311972d74c6d6df487aa95c80e30
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-09-02 12:32:39 +0200

    Run new app generator if no CLI params passed

--- packages/create-youtrack-app/index.js
+++ packages/create-youtrack-app/index.js
@@ -1,22 +1,41 @@
 #!/usr/bin/env node
 
-const { runner } = require('hygen')
-const Logger = require('hygen/dist/logger')
-const path = require('node:path')
-const defaultTemplates = path.join(__dirname, '_templates')
-const args = require('minimist')(process.argv.slice(2));
-const cwd = path.resolve(process.cwd(), args.cwd || '.');
+const { runner } = require("hygen");
+const { prompt } = require("enquirer");
+const Logger = require("hygen/dist/logger");
+const path = require("node:path");
+const defaultTemplates = path.join(__dirname, "_templates");
+const argv = process.argv.slice(2);
+const args = require("minimist")(argv);
+const cwd = path.resolve(process.cwd(), args.cwd || ".");
 
-console.log('Working directory:', cwd);
+(async function run() {
+  const hasHygenParams = ["init", "entity", "widget", "property"].some(
+    (key) => !!args[key]
+  );
 
-runner(process.argv.slice(2), {
-  templates: defaultTemplates,
-  cwd,
-  logger: new Logger.default(console.log.bind(console)),
-  createPrompter: () => require('enquirer'),
-  exec: (action, body) => {
-    const opts = body && body.length > 0 ? { input: body } : {}
-    return require('execa').shell(action, opts)
-  },
-  debug: true
-})
+  if (!hasHygenParams) {
+    const response = await prompt({
+      type: "confirm",
+      name: "new-app",
+      initial: true,
+      message: `New YouTrack App will be created in a directory "${cwd}".\n\nContinue?`,
+    });
+    if (response === false) {
+      return;
+    }
+    argv.unshift('init', 'vite-app');
+  }
+
+  runner(argv, {
+    templates: defaultTemplates,
+    cwd,
+    logger: new Logger.default(console.log.bind(console)),
+    createPrompter: () => require("enquirer"),
+    exec: (action, body) => {
+      const opts = body && body.length > 0 ? { input: body } : {};
+      return require("execa").shell(action, opts);
+    },
+    debug: true,
+  });
+})();
