bf1b7fc24d20e5422edbc6d90efc01b636b38c2a
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-09-04 11:59:03 +0200

    JT-81819 run `npm install` after generating and improve permanent token explaination

--- packages/create-youtrack-app/index.js
+++ packages/create-youtrack-app/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 const { runner } = require("hygen");
-const { Confirm } = require('enquirer');
+const execa = require("execa");
+const { Confirm } = require("enquirer");
 const Logger = require("hygen/dist/logger");
 const path = require("node:path");
 const defaultTemplates = path.join(__dirname, "_templates");
@@ -17,15 +18,15 @@ function runHygen(hygenArgs = argv) {
     createPrompter: () => require("enquirer"),
     exec: (action, body) => {
       const opts = body && body.length > 0 ? { input: body } : {};
-      return require("execa").shell(action, opts);
+      return execa.shell(action, opts);
     },
-    debug: !!process.env.DEBUG
+    debug: !!process.env.DEBUG,
   });
 }
 
 (async function run() {
-  const hasHygenParams = ["init", "entity", "widget", "property", "help"].some((key) =>
-    new Set(argv).has(key)
+  const hasHygenParams = ["init", "entity", "widget", "property", "help"].some(
+    (key) => new Set(argv).has(key)
   );
 
   // If some hygen-related params passed in, we call generator directly
@@ -42,40 +43,51 @@ function runHygen(hygenArgs = argv) {
     return;
   }
 
-  const appRes = await runHygen(['init', 'vite-app', ...argv]);
+  const appRes = await runHygen(["init", "vite-app", ...argv]);
   if (!appRes.success) {
     return;
   }
 
   console.log(`
-======= Your App is created ========
+====================================
 
 Now let's add first widget!
 You can add more later by running "npx @jetbrains/create-youtrack-app widget add"
 
 ====================================
-  `)
-  await runHygen(['widget', 'add', ...argv]);
+  `);
+  await runHygen(["widget", "add", ...argv]);
 
   if (
-    !(await new Confirm({
+    await new Confirm({
       initial: true,
       message: `Would you like your App to have Settings (you can do it later by running "npx @jetbrains/create-youtrack-app init settings")`,
-    }).run())
+    }).run()
   ) {
-    return;
+    await runHygen(["init", "settings", ...argv]);
   }
 
-  await runHygen(['init', 'settings', ...argv]);
+  console.log(`
+======= Your App is created! =======
+
+Now we are installing dependencies by running "npm install":
+====================================
+`);
+
+  await execa("npm", ["install"], {cwd}).stdout.pipe(process.stdout);
 
   console.log(`
-Done. Now run:
+Done!
 
-1. npm install
-2. npm run build
-3. npm run upload -- --host http://your-youtrack.url
-4. Follow command output to generate API token
+Now you would need to create a Permanent Token to upload the app into YouTrack isntance
+
+See documentation https://www.jetbrains.com/help/youtrack/server/manage-permanent-token.html
+
+Once you have this token, you could build and upload the App:
+
+1. npm run build
+2. npm run upload -- --host http://your-youtrack.url --token perm:cm9
 
 You can add more features to your app by running generator again. See help:`);
-  await runHygen(['help', 'show', ...argv]);
+  await runHygen(["help", "show", ...argv]);
 })();
