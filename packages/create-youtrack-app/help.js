0ee1be5abae8abb429c41d993cde18e3eaef9480
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-09-06 11:40:03 +0200

    chore: fix wrong command in help

--- packages/create-youtrack-app/help.js
+++ packages/create-youtrack-app/help.js
@@ -15,7 +15,7 @@ And follow the promts
 Once App is generated, one may need to add more features. See list of awailable commands:
 
 * ${chalk.magenta('npx @jetbrains/create-youtrack-app --help')} to see list of possible commands
-* ${chalk.magenta('npx @jetbrains/create-youtrack-app settings add')} to add a settings declaration (${chalk.underline('https://www.jetbrains.com/help/youtrack/devportal-apps/app-settings.html')})
+* ${chalk.magenta('npx @jetbrains/create-youtrack-app settings init')} to create a settings declaration (${chalk.underline('https://www.jetbrains.com/help/youtrack/devportal-apps/app-settings.html')})
 * ${chalk.magenta('npx @jetbrains/create-youtrack-app settings add')} to add one more property into Settings, created by command above
 * ${chalk.magenta('npx @jetbrains/create-youtrack-app widget add')} to add one more widget (${chalk.underline('https://www.jetbrains.com/help/youtrack/devportal-apps/apps-reference-extension-points.html')})
 * ${chalk.magenta('npx @jetbrains/create-youtrack-app extension-property add')} to add extension property (${chalk.underline('https://www.jetbrains.com/help/youtrack/devportal-apps/apps-extension-properties.html')})
