c3883725d6525ee7d3309b51e121f4c7efda5ded
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-09-27 17:10:51 +0200

    JT-82114 make tests run

--- packages/issue-dashboard-widgets/test-bundler.js
+++ packages/issue-dashboard-widgets/test-bundler.js
@@ -5,13 +5,19 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import chaiDOM from 'chai-dom';
 import sinonChai from 'sinon-chai';
+import spies from 'chai-spies';
 import chaiEnzyme from 'chai-enzyme';
 import sinon from 'sinon';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({adapter: new Adapter()});
 
 chai.use(chaiAsPromised);
 chai.use(chaiDOM);
 chai.use(sinonChai);
 chai.use(chaiEnzyme());
+chai.use(spies);
 
 Object.assign(window, {
   sinon,
@@ -19,6 +25,5 @@ Object.assign(window, {
   should: chai.should()
 });
 
-const testsContext = require.context('./src', true, /\.test\.js$/);
-
+const testsContext = require.context('./widgets', true, /\.test\.js$/);
 testsContext.keys().forEach(testsContext);
