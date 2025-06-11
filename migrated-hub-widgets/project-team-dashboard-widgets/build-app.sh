3cac6fcaa66a0bcd8cb7ed1846f936f8286071b6
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2025-06-11 09:25:49 +0200

    JT-89900 drop old built "project-team-dashboard-widgets" package

--- migrated-hub-widgets/project-team-dashboard-widgets/build-app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Packs the app content into zip archive that can be imported to YouTrack
-CURRENT_DIR=${PWD##*/}
-[ -f "$CURRENT_DIR".zip ] && rm "$CURRENT_DIR".zip
-zip -r "$CURRENT_DIR".zip . -x "*.zip" "*.iml" "*.sh" ".gitignore" "*.md" "*.idea*"
